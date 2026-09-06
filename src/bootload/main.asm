ca03c381952296869416c2963c95fdeb71d464c3
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-09-06 11:55:28 +0900

    Update main.asm

--- src/bootload/main.asm
+++ src/bootload/main.asm
@@ -1,1 +1,48 @@
+bits 16
+org 0x7C00
 
+start:
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+
+    mov [boot_drive], dl
+
+    ; Stage 2 をセクタ2～17へ読み込む
+    mov ah, 0x02
+    mov al, 16
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, [boot_drive]
+    mov bx, 0x8000
+    int 0x13
+    jc disk_error
+
+    jmp 0x0000:0x8000
+
+disk_error:
+    mov si, msg_error
+
+.print:
+    lodsb
+    test al, al
+    jz .halt
+    mov ah, 0x0E
+    int 0x10
+    jmp .print
+
+.halt:
+    cli
+    hlt
+    jmp .halt
+
+
+boot_drive db 0
+msg_error db "Disk error!", 0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
